f93ca28e2e3b4c1d0001de12536c784c07d2fe91
ccasson <colincasson80@gmail.com>
2025-05-21 14:56:36 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
-import "../styles/globals.css";
+import { Inter } from 'next/font/google'
+import { useEffect } from 'react'
+import '../styles/globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '600', '800'],
+  variable: '--font-inter',
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // Smooth scroll to hash on page load
+  useEffect(() => {
+    const hash = window.location.hash
+    if (hash) {
+      setTimeout(() => {
+        const element = document.querySelector(hash)
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' })
+        }
+      }, 100)
+    }
+  }, [])
+
+  return (
+    <main className={`${inter.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
