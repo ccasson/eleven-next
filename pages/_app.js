c48f9f4e9deab9702b30db10d28e4e5221e94424
ccasson <colincasson80@gmail.com>
2025-05-22 12:17:24 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,37 @@
-import '../styles/globals.css';
-import { Inter } from 'next/font/google';
+import '../styles/globals.css'
+import { Inter } from 'next/font/google'
+import { motion, AnimatePresence } from 'framer-motion'
+import Head from 'next/head'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 const inter = Inter({
   subsets: ['latin'],
+  weight: ['400', '600', '800'],
   variable: '--font-inter',
-});
+})
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
-    <main className={`${inter.variable} font-sans`}>
-      <Component {...pageProps} />
-    </main>
-  );
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div className={`${inter.variable} font-sans`}>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={router.route}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </div>
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
